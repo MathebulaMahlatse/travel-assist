7d51da81a792b4950bc99597eab96e31629e3ab3
Mahlatse Mathebula <mathebula@africandeveloper.co.za>
2018-07-26 20:37:53 +0200

    change index to work with material ui, ...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { Provider } from 'react-redux';
+import { applyMiddleware, compose, createStore } from 'redux';
+import reducers from './store/createStore';
+import SearchPage from './pages/searchPage';
+import thunk from 'redux-thunk';
+
+const theme = createMuiTheme({
+    
+  });
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+class App extends React.Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <MuiThemeProvider theme={theme}>
+                    <SearchPage/>
+                </MuiThemeProvider>
+            </Provider>
+        
+        )
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
